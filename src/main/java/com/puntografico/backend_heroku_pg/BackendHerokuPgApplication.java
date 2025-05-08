7906b132f398940ed64f9f50f9505f801f1c7158
benimhoff <simplementebennett@gmail.com>
2025-05-08 08:34:13 -0300

    "commit 2"

--- src/main/java/com/puntografico/backend_heroku_pg/BackendHerokuPgApplication.java
+++ src/main/java/com/puntografico/backend_heroku_pg/BackendHerokuPgApplication.java
@@ -8,6 +8,7 @@ public class BackendHerokuPgApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendHerokuPgApplication.class, args);
+		System.out.println("Hola!");
 	}
 
 }
