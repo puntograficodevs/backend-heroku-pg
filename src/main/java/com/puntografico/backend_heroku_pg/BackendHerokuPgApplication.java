8990f5d4685a31c85a61a18d32fd584030f0356f
benimhoff <simplementebennett@gmail.com>
2025-05-08 08:28:39 -0300

    primer commit

---
+++ src/main/java/com/puntografico/backend_heroku_pg/BackendHerokuPgApplication.java
@@ -0,0 +1,13 @@
+package com.puntografico.backend_heroku_pg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendHerokuPgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendHerokuPgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/puntografico/backend_heroku_pg/BackendHerokuPgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puntografico.backend_heroku_pg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendHerokuPgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
